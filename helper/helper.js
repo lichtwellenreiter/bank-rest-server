e2dca4600b456766a2a1941f54d66f7ef6954b5b
Florian Thiévent <florianjeremias@gmail.com>
2019-03-18 11:52:38 +0100

    init

---
+++ helper/helper.js
@@ -0,0 +1,32 @@
+// helper.js
+// ===============
+
+const uuidv1 = require('uuid/v1');
+
+module.exports = {
+
+    /**
+     * Prints a fancy Header to the console on startup
+     */
+    printHead: function () {
+        console.log('          :::::::::      :::     :::::::::  :::    :::  ::::::::   ');
+        console.log('         :+:    :+:   :+: :+:   :+:    :+: :+:    :+: :+:    :+:   ');
+        console.log('        +:+    +:+  +:+   +:+  +:+    +:+ +:+    +:+ +:+           ');
+        console.log('       +#++:++#+  +#++:++#++: +#++:++#+  +#++:++#++ +#++:++#++     ');
+        console.log('      +#+    +#+ +#+     +#+ +#+    +#+ +#+    +#+        +#+      ');
+        console.log('     #+#    #+# #+#     #+# #+#    #+# #+#    #+# #+#    #+#       ');
+        console.log('    #########  ###     ### #########  ###    ###  ########         ');
+        console.log('  ===============================================================  ');
+        console.log('                                                                   ');
+        console.log('                 Bank Assignment Backend HTTP Server               ');
+        console.log('                      Made with ❤️ and node.js                     ');
+        console.log('                        by lichtwellenreiter.                      ');
+        console.log('                                                                   ');
+        console.log('  ===============================================================  ');
+        console.log('                                                                   ');
+    },
+
+    createAccId: function () {
+        return uuidv1();
+    }
+};
